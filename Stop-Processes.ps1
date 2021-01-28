fa6c87e9d000a2f82bd0bb93419302ac20dde253
Sowat-Hossain-Rafi <sowat.hossain.rafi@g.bracu.ac.bd>
2021-01-28 07:41:26 +0600

    Remove the messenger part.

--- Stop-Processes.ps1
+++ Stop-Processes.ps1
@@ -1,7 +1,5 @@
 Stop-Process -Name "discord"
 Write-Host "`nDiscord has been stopped.`n"
-Stop-Process -Name "messenger"
-Write-Host "`nMessenger has been stopped.`n"
 Stop-Process -Name "slack"
 Write-Host "`nSlack has been stopped.`n"
 Stop-Process -Name "AdobeIPCBroker"
